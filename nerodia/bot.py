b9127e8d2a024830d497ae890288855af34b09f3
Volcyy <Johannes.christ@gmail.com>
2017-08-08 00:01:25 +0200

    Improve the "wait for signal" implementation.

--- nerodia/bot.py
+++ nerodia/bot.py
@@ -1,24 +1,40 @@
-import workers
+"""
+The startup script for Nerodia.
+Starts off the Threads, one for
+each class defined in workers.py.
+After starting them, it sleeps
+for up to one year, effectively
+waiting for any signal to interrupt
+it. On interruption, it puts None
+onto the event queue in order for
+the Reddit consumer to stop
+processing input events and also
+calls the `stop` method on the
+other threads to make them
+exit their while polling loop.
+"""
 
+import time
+
+from . import workers
+
+SECONDS_IN_A_YEAR = 60 * 60 * 24 * 365
 
 if __name__ == '__main__':
-    threads = [
+    THREADS = [
         workers.RedditConsumer(),
         workers.RedditProducer(),
         workers.TwitchProducer()
     ]
 
-    for t in threads:
+    for t in THREADS:
         t.start()
 
-    try:
-        input("Press any key to stop...")
-    except KeyboardInterrupt:
-        pass
-    finally:
-        print("Stopping Workers...")
-        workers.event_queue.put(None)
-        for t in threads:
-            t.stop()
-            t.join()
+    time.sleep(SECONDS_IN_A_YEAR)
 
+    print("Stopping Workers...")
+    workers.event_queue.put(None)
+
+    for t in THREADS:
+        t.stop()
+        t.join()

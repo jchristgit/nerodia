d1dc54d2a331d3bc5d11764690422c7b0c996b71
Volcyy <Johannes.christ@gmail.com>
2017-08-10 13:09:12 +0200

    Adds a test for the get_subreddit_mods funcition.

--- test/test_database.py
+++ test/test_database.py
@@ -8,9 +8,11 @@ or the Twitch API.
 """
 
 from . import setup
+from praw.models import Redditor, RedditorList
+
 setup.init()
 
-from nerodia import database as db
+from nerodia import database as db  # noqa
 
 
 def test_unknown_stream():
@@ -49,6 +51,23 @@ def test_subreddit_exists():
     assert not db.subreddit_exists("thisdefinitelydoesnotexist")
     assert not db.subreddit_exists("123912i0491528190518")
     assert not db.subreddit_exists("anotherunknownsubreddit")
+
+
+def test_subreddit_moderators():
+    """
+    Validates that the get_subreddit_moderators
+    function properly returns a list of Moderators
+    for the given Subreddit, and `None` if the
+    Subreddit was not found.
+    """
+
+    zero_mods = db.get_subreddit_moderators("deadsub")
+    assert len(zero_mods) == 0
+
+    three_mods = db.get_subreddit_moderators("bottiquette")
+    assert len(three_mods) == 3
+    assert isinstance(three_mods, RedditorList)
+    assert all(isinstance(mod, Redditor) for mod in three_mods)
 
 
 setup.finish()

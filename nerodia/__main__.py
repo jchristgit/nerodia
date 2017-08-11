ad4fef17796398cc2f8b0d0c4d10b8b44d7fa93b
Volcyy <Johannes.christ@gmail.com>
2017-08-11 16:56:37 +0200

    Adds `__main__.py` for running the module directly.

---
+++ nerodia/__main__.py
@@ -0,0 +1,46 @@
+"""
+The entry point for Nerodia.
+Starts off the Threads, one for
+each class defined in workers.py.
+After starting them, it sleeps
+for up to one year, effectively
+waiting for any signal to interrupt
+it. On interruption, it puts None
+onto the event queue in order for
+the Reddit consumer to stop
+processing input events and also
+calls the `stop` method on the
+other threads to make them
+exit their while polling loop.
+"""
+
+import time
+
+from . import workers
+from .clients import discord as discord_bot
+
+SECONDS_IN_A_YEAR = 60 * 60 * 24 * 365
+
+if __name__ == '__main__':
+    THREADS = [
+        workers.RedditConsumer(),
+        workers.RedditProducer(),
+        workers.TwitchProducer()
+    ]
+
+    for t in THREADS:
+        t.start()
+
+    discord_bot.run()
+
+    try:
+        time.sleep(SECONDS_IN_A_YEAR)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print("Stopping Workers...")
+        workers.event_queue.put(None)
+
+        for t in THREADS:
+            t.stop()
+            t.join()

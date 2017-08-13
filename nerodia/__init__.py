296002b47312843b3144da61f6db970721c0dd58
Volcyy <Johannes.christ@gmail.com>
2017-08-13 19:18:08 +0200

    Adds cog and util to init for relative imports.

--- nerodia/__init__.py
+++ nerodia/__init__.py
@@ -1,9 +1,11 @@
 # flake8: noqa
 from . import bot
 from . import clients
+from . import cog
 from . import database
 from . import handlers
 from . import models
 from . import poller
 from . import storage
+from . import util
 from . import workers

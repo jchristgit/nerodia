e66383e21d70b09911763a6584ab592ca745ed91
Volcyy <Johannes.christ@gmail.com>
2017-08-13 16:55:37 +0200

    Fix scope issue with the token Embed.

--- nerodia/cog.py
+++ nerodia/cog.py
@@ -5,28 +5,39 @@ for the Discord Bot.
 
 import datetime
 import discord
-import time
 
 from discord.ext import commands
 
+from . import util
+
 
 PM_URL = "https://www.reddit.com/message/compose?to=Botyy&subject=verification"
-CR_INSTRUCTIONS = discord.Embed(
-    title="Connect your Reddit Account",
-    colour=discord.Colour.blue(),
-    timestamp=datetime.datetime.now()
-).add_field(
-    name="Disclaimer",
-    value="By connecting your account, you agree that your "
-          "**Discord ID is stored unencrypted for an indefinite "
-          "time, along with your Reddit name, and this information "
-          "may appear in the bot's log messages**. You can "
-          "disconnect a connected account at any time.",
-    inline=False
-).add_field(
-    name="Instructions",
-    value="[Please send me a Reddit PM](PM_URL) containing the **Token**."
-)
+
+
+def create_instructions() -> discord.Embed:
+    """
+    Creates an Embed containing the disclaimer
+    for adding a Reddit account to your Discord account.
+    This should be used for adding a field with the token
+    which the user should send to the bot via a direct message.
+
+    Returns:
+        discord.Embed: An embed with a disclaimer about user data.
+    """
+
+    return discord.Embed(
+        title="Connect your Reddit Account",
+        colour=discord.Colour.blue(),
+        timestamp=datetime.datetime.now()
+    ).add_field(
+        name="Disclaimer",
+        value="By connecting your account, you agree that your "
+              "**Discord ID is stored unencrypted for an indefinite "
+              "time, along with your Reddit name, and this information "
+              "may appear in the bot's log messages**. You can "
+              "disconnect a connected account at any time.",
+        inline=False
+    )
 
 
 class Nerodia:
@@ -44,7 +55,15 @@ class Nerodia:
         instructions that this command sends upon invocation.
         """
 
-        await ctx.send(embed=CR_INSTRUCTIONS)
+        token = util.random_string()
+        instructions = create_instructions()
+        instructions.add_field(
+            name="Instructions",
+            value=f"Send me a [Reddit Message]({PM_URL}) containing"
+                  f"`{token}` to verify your account.",
+            inline=False
+        )
+        await ctx.send(embed=instructions)
 
 
 def setup(bot):

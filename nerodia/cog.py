3704bd8a13a58c324bded2cb39a992f46c7aa70d
Volcyy <Johannes.christ@gmail.com>
2017-08-13 16:13:06 +0200

    Adds basic verification instructions.

--- nerodia/cog.py
+++ nerodia/cog.py
@@ -3,9 +3,30 @@ Contains the command group
 for the Discord Bot.
 """
 
+import datetime
 import discord
+import time
 
 from discord.ext import commands
+
+
+PM_URL = "https://www.reddit.com/message/compose?to=Botyy&subject=verification"
+CR_INSTRUCTIONS = discord.Embed(
+    title="Connect your Reddit Account",
+    colour=discord.Colour.blue(),
+    timestamp=datetime.datetime.now()
+).add_field(
+    name="Disclaimer",
+    value="By connecting your account, you agree that your "
+          "**Discord ID is stored unencrypted for an indefinite "
+          "time, along with your Reddit name, and this information "
+          "may appear in the bot's log messages**. You can "
+          "disconnect a connected account at any time.",
+    inline=False
+).add_field(
+    name="Instructions",
+    value="[Please send me a Reddit PM](PM_URL) containing the **Token**."
+)
 
 
 class Nerodia:
@@ -22,6 +43,8 @@ class Nerodia:
         Please make sure to carefully read through the
         instructions that this command sends upon invocation.
         """
+
+        await ctx.send(embed=CR_INSTRUCTIONS)
 
 
 def setup(bot):

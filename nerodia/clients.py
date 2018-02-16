1602fde41a790620566dd141b95370004121fcf8
Johannes Christ <Johannes.christ@gmail.com>
2018-02-16 20:53:37 +0100

    Use more accurate naming.

--- nerodia/clients.py
+++ nerodia/clients.py
@@ -21,18 +21,18 @@ from .apis.twitch import TwitchClient
 with open("config.json") as f:
     as_json = json.load(f)
     DISCORD_CFG = as_json["discord"]
-    REDDIT_AUTH = as_json["reddit_auth"]
-    TWITCH_AUTH = as_json["twitch"]
+    REDDIT_CFG = as_json["reddit_auth"]
+    TWITCH_CFG = as_json["twitch"]
 
 discord_token = DISCORD_CFG["token"]
 discord_game = DISCORD_CFG["game"]
 
 reddit = praw.Reddit(
-    client_id=REDDIT_AUTH["client_id"],
-    client_secret=REDDIT_AUTH["client_secret"],
-    username=REDDIT_AUTH["username"],
-    password=REDDIT_AUTH["password"],
-    user_agent=REDDIT_AUTH["user_agent"]
+    client_id=REDDIT_CFG["client_id"],
+    client_secret=REDDIT_CFG["client_secret"],
+    username=REDDIT_CFG["username"],
+    password=REDDIT_CFG["password"],
+    user_agent=REDDIT_CFG["user_agent"]
 )
 
-twitch = TwitchClient(client_id=TWITCH_AUTH["client_id"])
+twitch = TwitchClient(client_id=TWITCH_CFG["client_id"])

05f846221515ffdea40b43a2ad11b88e895e58b3
Johannes Christ <Johannes.christ@gmail.com>
2018-02-16 23:51:53 +0100

    Fix up docstring.

--- nerodia/__main__.py
+++ nerodia/__main__.py
@@ -1,17 +1,5 @@
 """
 The entry point for Nerodia.
-Starts off the Threads, one for
-each class defined in workers.py.
-After starting them, it sleeps
-for up to one year, effectively
-waiting for any signal to interrupt
-it. On interruption, it puts None
-onto the event queue in order for
-the Reddit consumer to stop
-processing input events and also
-calls the `stop` method on the
-other threads to make them
-exit their while polling loop.
 """
 
 

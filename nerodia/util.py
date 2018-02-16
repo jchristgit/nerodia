36b905d322834f9041754d06270efa14a6fd2969
Johannes Christ <Johannes.christ@gmail.com>
2018-02-16 20:16:36 +0100

    Remove legacy code.

--- nerodia/util.py
+++ nerodia/util.py
@@ -9,17 +9,14 @@ import random
 import string
 
 
-# The Twitch stream states, as a stream name => stream online mapping.
-stream_states = dict()
-
 # A Discord ID -> verification token mapping.
 token_dict = dict()
 
-# Used to verify users. To be used along with the lock defined below.
+# Used to verify users.
 verify_dict = dict()
 
 
-def random_string(length: int = 15):
+def random_string(length: int=15):
     """
     Returns a string made up from
     random characters.
@@ -35,17 +32,3 @@ def random_string(length: int = 15):
     """
 
     return ''.join(random.choice(string.ascii_letters) for _ in range(length))
-
-
-def remove_token(user_id: str):
-    """
-    Removes the given user ID from the token dict,
-    along with the token which the user may or may not
-    have used for connecting his Discord account with
-    his reddit account for easy execution of commands.
-
-    Arguments:
-        user_id (str): The user ID which should be removed from the dict.
-    """
-
-    del token_dict[user_id]

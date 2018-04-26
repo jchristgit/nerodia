d8f17f0ca55ccb6012fa692a479bb800eae78185
Johannes Christ <Johannes.christ@gmail.com>
2018-04-26 16:02:53 +0200

    Re-add Twitch stream update handler.
    Signed-off-by: Johannes Christ <Johannes.christ@gmail.com>

--- nerodia/handlers.py
+++ nerodia/handlers.py
@@ -6,9 +6,18 @@ a user, or updating a Discord
 server about a stream update.
 """
 
-import praw
+import logging
+from typing import Optional
 
+import praw
+from discord.ext import commands
+
+from .database import guilds as guild_db
+from .twitch import TwitchStream
 from .util import token_dict, verify_dict
+
+
+log = logging.getLogger(__name__)
 
 
 def verify(msg: praw.models.Message):
@@ -38,10 +47,27 @@ def handle_message(msg: praw.models.Message) -> None:
     Handles the message Event and processes the new message.
     """
 
-    print('New Message from', (msg.author or msg.subreddit).name, 'content:', msg.body)
+    log.debug(
+        f"Got new message from {(msg.author or msg.subreddit).name!r}, contents: {msg.body!r}."
+    )
 
     if msg.subject == "verification":
         verify(msg)
     elif msg.body.startswith("**gadzooks!"):
         msg.subreddit.mod.accept_invite()
-        print(f"Accepted a Moderator invitation to {msg.subreddit}.")
+        log.info(f"Accepted a Moderator invitation to {msg.subreddit.name}.")
+
+
+async def handle_stream_update(bot: commands.Bot, stream_name: str, now_online: bool, stream: Optional[TwitchStream]):
+    followers = guild_db.get_guilds_following(stream_name)
+
+    for guild_id in followers:
+        update_channel_id = guild_db.get_update_channel(guild_id)
+        if update_channel_id is None:
+            log.warn(f"Guild {guild_id} is following {stream_name!r}, but has no update channel set.")
+        else:
+            channel = await discord_bot.get_channel(update_channel_id)
+            if channel is None:
+                log.warn(f"Guild {guild_id} has an update channel set, but it could not be found.")
+            else:
+                await channel.send(f"**{stream_name}** is now {'online' if now_online else 'offline'}!")
--- nerodia/workers.py
+++ nerodia/workers.py
@@ -3,15 +3,41 @@ Polls the Bot's Reddit inbox for new messages.
 """
 
 import asyncio
+import logging
 
-from .clients import reddit
-from .handlers import handle_message
+from discord.ext import commands
+
+from .clients import reddit, twitch
+from .database.common import get_all_follows
+from .handlers import handle_message, handle_stream_update
 
 
-async def inbox_poller():
-    print("[InboxPoller] Ready.")
+log = logging.getLogger(__name__)
+
+
+async def inbox_poller(bot: commands.Bot):
+    await bot.wait_until_ready()
+    log.info("Started reddit inbox poller.")
     while True:
         for msg in reddit.inbox.unread():
             handle_message(msg)
             msg.mark_read()
         await asyncio.sleep(10)
+
+
+async def stream_poller(bot: commands.Bot):
+    await bot.wait_until_ready()
+    log.info("Started Twitch stream poller.")
+
+    old_data = {}
+    while True:
+        all_follows = get_all_follows()
+        stream_information = await twitch.get_streams(*all_follows)
+        for username, stream in stream_information.items():
+            if old_data.get(username, default=stream) != stream:
+                is_online = stream is not None
+                log.info(f"Stream status for {username} changed, now {is_online}. Sending an update...")
+                await handle_stream_update(bot, username, is_online, stream)
+
+        await asyncio.sleep(10)
+        old_data = stream_information

144809dc2011dec8e55e34f654c2eb476fa729b2
Johannes Christ <Johannes.christ@gmail.com>
2018-04-26 14:17:55 +0200

    Remove `uvloop` initialization for now.

--- nerodia/__init__.py
+++ nerodia/__init__.py
@@ -1,13 +1,5 @@
 # flake8: noqa
 
-try:
-    import uvloop
-except ImportError:
-    pass
-else:
-    import asyncio
-    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-
 from . import (
     database,
     bot, cog, clients, handlers,

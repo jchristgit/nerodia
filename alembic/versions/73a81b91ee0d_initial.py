6afffa14b55aca148fc61830438e1ae0db083801
Johannes Christ <Johannes.christ@gmail.com>
2018-04-27 00:56:27 +0200

    Ignore unused imports in (now) empty migration.
    Signed-off-by: Johannes Christ <Johannes.christ@gmail.com>

--- alembic/versions/73a81b91ee0d_initial.py
+++ alembic/versions/73a81b91ee0d_initial.py
@@ -5,8 +5,8 @@ Revises:
 Create Date: 2018-04-25 21:33:13.841514
 
 """
-from alembic import op
-import sqlalchemy as sa
+from alembic import op  # noqa
+import sqlalchemy as sa  # noqa
 
 
 # revision identifiers, used by Alembic.
